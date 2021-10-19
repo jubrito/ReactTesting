9510f5bb09637fb4dbfa0e60e3eee8c64903d747
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-10-19 11:22:56 -0300

    Separates duplicate code using beforeEach

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,27 +4,25 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("when the app is rendered", () => {
+  beforeEach(() => {
+    render(<App/>);
+  })
   test("should render app without errors", () => {
-    render(<App />);
     screen.debug();
   })
   test("should select the children that is being passed to the CustomInput component", () => {
-    render(<App/>);
     screen.getByText('Input:'); // implicit assertion
 
   })
   test("should select input element by its role", () => {
-    render(<App/>);
     expect(screen.getByRole('textbox')).toBeInTheDocument(); // explicit assertion
   })
 
   test("should select a label element by its text", () => {
-    render(<App/>);
     screen.getByLabelText(/Input/);
   })
 
   test("should select input element by placeholder text", () => {
-    render(<App/>);
     screen.getByPlaceholderText('example');
   })
 })
