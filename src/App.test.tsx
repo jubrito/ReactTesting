27ac50e83e15ad3162edaf2b414e777651cbf525
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-10-19 10:42:45 -0300

    Renders a component with RTL

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("when the app is rendered", () => {
+  test("should render app without errors", () => {
+    render(<App />);
+    screen.debug();
+  })
+})
