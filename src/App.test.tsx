c1f32b0c26863e90363cb601fdaed9ce1698f172
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-10-19 10:42:58 -0300

    Selects elements with getByText

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,4 @@
+// Implicit assertion = assert without using expect statement
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
@@ -6,5 +7,11 @@ describe("when the app is rendered", () => {
   test("should render app without errors", () => {
     render(<App />);
     screen.debug();
+  })
+  test("should select the children that is being passed to the CustomInput component", () => {
+    render(<App/>);
+  
+    screen.getByText('Input:'); // implicit
+    // expect(screen.getByText('Input:')).toBeInTheDocument(); // not implicit
   })
 })
